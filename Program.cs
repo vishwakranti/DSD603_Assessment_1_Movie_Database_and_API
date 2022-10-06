391d67b409e947e247675329bc8e9b4854899e0f
vishwakranti <87359394+vishwakranti@users.noreply.github.com>
2022-10-06 20:12:39 +1300

    Fixed reference looping for api when including related tables

--- Program.cs
+++ Program.cs
@@ -11,8 +11,11 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    ;
+builder.Services.AddControllersWithViews()
+    .AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles);
+
 
 var app = builder.Build();
 
